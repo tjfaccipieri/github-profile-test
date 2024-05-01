e4a77fc2493c6cfe151b4e980a78e1fc5497f76a
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2024-05-01 12:39:59 -0300

    fetch de repos

--- src/components/ProfileData.tsx
+++ src/components/ProfileData.tsx
@@ -1,27 +1,43 @@
 import { useEffect, useState } from "react";
 
 function ProfileData() {
-  const [response, setResponse] = useState<any>({})
+  const [user, setUser] = useState<any>({})
+  const [repos, setRepos] = useState<any[]>([])
+
 
   async function getDataFromGithub(){
     const data = await fetch('https://api.github.com/users/tjfaccipieri')
     const res = await data.json()
-    setResponse(res)
+    setUser(res)
+  }
+
+  async function getUserRepos(){
+    const data = await fetch('https://api.github.com/users/tjfaccipieri/repos')
+    const res = await data.json()
+    setRepos(res)
   }
 
   useEffect(() => {
     getDataFromGithub()
+    getUserRepos()
   }, [])
 
   return (
+    <>
     <div className="mx-auto container flex p-4 gap-8 items-center bg-slate-800">
-      <img src={response.avatar_url} alt="" className="rounded-full w-44 border-white border-2 p-2" />
+      <img src={user.avatar_url} alt="" className="rounded-full w-44 border-white border-2 p-2" />
       <section className="flex flex-col gap-2">
-        <h2 className="text-5xl font-extrabold font-mono">{response.name}</h2>
-        <p className="text-3xl font-bold font-mono">/{response.login}</p>
-        <p>Total public repositories: {response.public_repos} / Total followers: {response.followers}</p>
+        <h2 className="text-5xl font-extrabold font-mono">{user.name}</h2>
+        <p className="text-3xl font-bold font-mono">/{user.login}</p>
+        <p>Total public repositories: {user.public_repos} / Total followers: {user.followers}</p>
       </section>
     </div>
+    <div className="bg-teal-800">
+      {repos.map(repo => (
+        <p>{repo.name}</p>
+      ))}
+    </div>
+    </>
   )
 }
 
